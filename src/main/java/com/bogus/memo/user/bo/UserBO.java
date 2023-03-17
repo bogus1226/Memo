12cfcb1faedc0e7eddff7e35c86acda5bc00aef8
user <user>
2023-03-17 17:48:24 +0900

    [염근효] - 회원 가입 화면 구성 - 회원 가입 api 추가 - 비밀번호 암호화 기능 추가
    bogus1226/Memo#1

---
+++ src/main/java/com/bogus/memo/user/bo/UserBO.java
@@ -0,0 +1,25 @@
+package com.bogus.memo.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bogus.memo.common.EncryptService;
+import com.bogus.memo.user.dao.UserDAO;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserDAO userDAO;
+
+	public int addUser(
+			String loginId
+			, String password
+			, String name
+			, String email) {
+		
+		String encryptPassword = EncryptService.md5(password);
+		
+	 	return userDAO.insertUser(loginId, encryptPassword, name, email);
+	}
+}
