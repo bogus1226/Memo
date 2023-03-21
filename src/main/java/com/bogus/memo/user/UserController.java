98cd275e52ac87089d9cfbb66239ff6caf517bb0
user <user>
2023-03-21 16:20:34 +0900

    [염근효] - 로그아웃 기능!!

--- src/main/java/com/bogus/memo/user/UserController.java
+++ src/main/java/com/bogus/memo/user/UserController.java
@@ -1,5 +1,8 @@
 package com.bogus.memo.user;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +20,15 @@ public class UserController {
 	public String signinInput() {
 		
 		return "user/signin";
+	}
+	
+	@GetMapping("/signout")
+	public String signout(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("userId");
+		session.removeAttribute("userName");
+		
+		return "redirect:/user/signin/view";
 	}
 }
