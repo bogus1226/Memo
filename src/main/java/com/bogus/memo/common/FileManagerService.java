2d5f37ec68a438c89db70aa6df0302da7c65f4ab
user <user>
2023-03-23 18:52:43 +0900

    [염근효] -  올리기 했을때 파일을 안올려도 게시물 업로드 가능하게 구현
    사진추가기능 #4

--- src/main/java/com/bogus/memo/common/FileManagerService.java
+++ src/main/java/com/bogus/memo/common/FileManagerService.java
@@ -15,6 +15,10 @@ public class FileManagerService {
 	// 파일 저장 -> 경로 생성
 	
 	public static String saveFile(int userId, MultipartFile file) {
+		
+		if(file == null) {
+			return null;
+		}
 	
 		// 사용자 별로 폴더를 구분
 		// 시간을 포함해서 폴더를 구분
